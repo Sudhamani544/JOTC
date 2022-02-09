64ff749d901dea8ca7778a60f45c2a9b6709fbf0
sudha <ksudha544@gmail.com>
2022-02-09 19:48:53 +0100

    chaged pool to client

--- server/src/db.js
+++ server/src/db.js
@@ -1,30 +1,40 @@
 const Pool = require("pg").Pool;
 const dotenv = require("dotenv");
+const { Client } = require("pg");
 
 dotenv.config({ path: ".env" });
 
-const devConfig = {
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PWD,
-  host: process.env.DATABASE_HOST,
-  port: process.env.DATABASE_PORT,
-  database: process.env.DATABASE,
-};
+// const devConfig = {
+//   user: process.env.DATABASE_USER,
+//   password: process.env.DATABASE_PWD,
+//   host: process.env.DATABASE_HOST,
+//   port: process.env.DATABASE_PORT,
+//   database: process.env.DATABASE,
+// };
 
-const prodConfig = {
+// const prodConfig = {
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: false, //heroku addons
+// };
+
+// const pool = new Pool(
+//   process.env.NODE_ENV === "production" ? prodConfig : devConfig
+// );
+
+// pool
+//   .connect()
+//   .then(() => {
+//     console.log("connected to pg");
+//   })
+//   .catch((err) => console.log(err));
+
+const client = new Client({
   connectionString: process.env.DATABASE_URL,
-  ssl: false, //heroku addons
-};
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
-const pool = new Pool(
-  process.env.NODE_ENV === "production" ? prodConfig : devConfig
-);
+client.connect();
 
-pool
-  .connect()
-  .then(() => {
-    console.log("connected to pg");
-  })
-  .catch((err) => console.log(err));
-
-module.exports = pool;
+module.exports = client;
